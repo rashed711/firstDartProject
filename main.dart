df6a1ed6e20de2846bc41a9ccd7e52f2ef75ccd6
Rashed <rashed1711@gmail.com>
2025-02-24 16:44:17 +0200

    first Task of Course

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
 void main() {
   List<int> students = [7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-  int studentAge = 14;
+  int studentAge = 10;
 
   if (studentAge == students[0]) {
     print('الصف الثاني الابتدائي');
